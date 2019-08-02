d4fb0f53da92560f7d2c4b0f10113e0a613cde27
Max Aubain <max.aubain@gmail.com>
2019-08-02 17:11:49 +0200

    Configures rspec related configuration files for use with Shoulda-Matchers and disables default testing tools.

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,18 @@ module DTCScreenshot
     config.generators.system_tests = nil
   end
 end
+
+class Application < Rails::Application
+  config.load_defaults 5.1
+
+  # Disable generation of helpers, javascripts, css, and view, helper, routing and controller specs
+  config.generators do |generate|
+    generate.helper false
+    generate.assets false
+    generate.view_specs false
+    generate.helper_specs false
+    generate.routing_specs false
+    generate.controller_specs false
+    generate.system_tests false
+  end
+end
