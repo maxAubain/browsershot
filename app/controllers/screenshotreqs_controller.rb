23dafe946a3480535ecaf2602eb00f07c58f3b20
Max Aubain <max.aubain@gmail.com>
2019-09-07 17:51:08 +0200

    Refactors comments in screenshotreqs controller and refactors hard-coded image paths and file names

--- app/controllers/screenshotreqs_controller.rb
+++ app/controllers/screenshotreqs_controller.rb
@@ -1,35 +1,30 @@
+# frozen_string_literal: true
+
 class ScreenshotreqsController < ApplicationController
-  # Screenshot requests (Screenshotreqs for short) Controller.  
-  # This is the main controller for the application that governs CRUD
-  # functions for Screenshot requests and Screenshots.
-  
+  # This controller governs CRUD functions for Screenshotreqs
+  # (short for "screenshot requests") and Screenshots.
+
   def index
-    # index method sends all screenshot request params to the front page: 
-    # ./app/views/screenshotreqs/index.html.erb
-    
     @screenshotreqs = Screenshotreq.all
+    # index method sends all screenshot request params to the front page: index.html.erb
   end
-  
-  # Do I need this method??
+
   def new
     @screenshotreq = Screenshotreq.new
+    # Enables create method
   end
 
   def create
-    # Create method creates a new instance of a Screenshot Request 
-    # to be populated with data submitted from the form in new.html.erb.  
-    # When the "submit request" button on the form is clicked, the method 
-    # attempts to save the screenshotreq. If the new screenshotreq meets 
-    # the validation criteria in ./app/models/screenshotreq.rb, then .save 
-    # is successful and parse_urls and create_screenshots methods are run.  Also
-    # after save, the view redirects to the home page.
-    
     @screenshotreq = Screenshotreq.new(screenshotreq_params)
-    
+    # Creates a new instance of a screenshotreq with data submitted from the form in new.html.erb.
+    # When the "submit request" button on the form is clicked, the input URLs and request name
+    # is checked against the model validation criteria.
+
     if @screenshotreq.save
+      # If validation criteria is met, save is successful and...
       parse_urls
-      # parse URLs
-      
+      # URLs from the list of URLs submitted in the form are parsed
+
       create_screenshots
       # create and save screenshots
 
@@ -39,23 +34,17 @@ class ScreenshotreqsController < ApplicationController
   end
 
   def show
-    # show method reads the information of a previously saved screenshot 
-    # request in the database, and sends that information to a new view 
-    # page defined by ./app/views/screenshotreqs/show.html.erb. The particular 
-    # screenshotreq read from the database is identified by id parameter.
-
     @screenshotreq = Screenshotreq.find(params[:id])
+    # Show method finds saved screenshotreq data and sends to a new view configured in show.html.erb.
   end
 
   private
-  # private methods (below this point) not accessible from calls outside 
-  # of this controller.
+
+  # private methods arenot accessible from calls beyond this controller.
 
   def screenshotreq_params
-    # screenshot_params method is used to enable the create method to 
-    # save screenshot requests with name and urls params.
-
     params.require(:screenshotreq).permit(:name, :urls)
+    # Enable the create method to save screenshot requests with name and urls params.
   end
 
   def parse_urls
@@ -63,15 +52,17 @@ class ScreenshotreqsController < ApplicationController
     # and assigns each individual URL in the list to a separate Screenshot instance.
 
     urls = []
-    urls.push(@screenshotreq.urls.gsub(/\s+/, ""))
+    urls.push(@screenshotreq.urls.gsub(/\s+/, ''))
     # Puts URLs string into an array and removes any whitespace from string
 
-    urls_split = urls[0].split(";")
+    urls_split = urls[0].split(';')
     # Turns the URLs string into an array of single URL strings
 
     counter = 1
     urls_split.each do |url|
-      @screenshot = Screenshot.create(url: url, img_path: "./storage/req" + @screenshotreq.id.to_s + "shot" + counter.to_s + ".png", img_path_short: "req" + @screenshotreq.id.to_s + "shot" + counter.to_s + ".png")
+      image_path = "./storage/req#{@screenshotreq.id}shot#{counter}.png"
+      image_file_name = "req#{@screenshotreq.id}shot#{counter}.png"
+      @screenshot = Screenshot.create(url: url, img_path: image_path, img_path_short: image_file_name)
       @screenshot.screenshotreq = @screenshotreq
       @screenshot.save!
       counter += 1
@@ -80,12 +71,11 @@ class ScreenshotreqsController < ApplicationController
     # url is the parameter used by the screenshot capture method which determines the webpage to screenshot,
     # img_path is the location where the image file of the screenshot will be stored, and
     # img_path_short is the name of the image file.
-
   end
 
   def create_screenshots
-  # create_screenshots uses helper methods provided by the Webshot Gem to obtain
-  # image files of screenshots at specified URLs.
+    # create_screenshots uses helper methods provided by the Webshot Gem to obtain
+    # image files of screenshots at specified URLs.
 
     ws = Webshot::Screenshot.instance
     # instantiate an instance of Webshot
