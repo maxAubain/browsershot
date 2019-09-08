579391675af303137bdb500662aced76ec8564df
Max Aubain <max.aubain@gmail.com>
2019-09-08 04:15:46 +0200

    Adds shell request spec file

--- spec/factories/screenshots.rb
+++ spec/factories/screenshots.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :screenshot do
     url { "https://www.google.com/" }
-    img_path { "./app/assets/images/img.png" }
+    img_path { "./storage/img.png" }
     img_path_short { "img.png"}
-    screenshotreq
+    screenshotreq # Generates associates screenshotreq.
   end
 end
---
+++ spec/requests/request_link_spec.rb
@@ -0,0 +1,16 @@
+# Click on "make a new request" link.  View goes from localhost:3000 to localhost:3000/screenshotreqs/new.
+require 'rails_helper'
+
+RSpec.describe "Screenshotreqs", type: :request do
+  
+  describe '' do
+    it '' do
+    end
+  end
+
+  describe '' do
+    it '' do
+    end
+  end
+
+end
