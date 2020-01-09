df04fa635beef4884608ade8e15ed3a70b7e0ae1
Max Aubain <maxaubain@protonmail.com>
2020-01-09 14:10:11 -0500

    Refactors 'webshot' gem functionality with 'ruby_webshot' gem

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ ruby '2.6.3'
 gem 'rails', '~> 5.2.3'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.12'
-gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 
 gem 'coffee-rails', '~> 4.2'
@@ -16,25 +15,28 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 # Screenshot capture
+#old
 gem 'webshot', '~> 0.1.1'
+#new
+gem 'ruby_webshot', '~> 0.0.6'
+gem 'webdrivers', '~> 4.2'
 
 # For future use to enable async methods regarding generation of screen shots
 # gem 'concurrent-ruby', '~> 1.1', '>= 1.1.5'
 
-
 group :development, :test do
+  gem 'cucumber-rails', require: false
+  gem 'database_cleaner'
   gem 'factory_bot_rails'
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'rspec-rails'
   gem 'shoulda-matchers'
-  gem 'cucumber-rails', require: false
-  gem 'database_cleaner'
 end
 
 group :development do
-  gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'web-console', '>= 3.3.0'
 end
--- app/controllers/screenshotreqs_controller.rb
+++ app/controllers/screenshotreqs_controller.rb
@@ -6,7 +6,7 @@ class ScreenshotreqsController < ApplicationController
 
   def index
     @screenshotreqs = Screenshotreq.all
-    # index method sends all screenshot request params to the front page: 
+    # index method sends all screenshot request params to the front page:
     # index.html.erb
   end
 
@@ -17,9 +17,9 @@ class ScreenshotreqsController < ApplicationController
 
   def create
     @screenshotreq = Screenshotreq.new(screenshotreq_params)
-    # Creates a new instance of a screenshotreq with data submitted from 
-    # the form in new.html.erb. When the "submit request" button on the 
-    # form is clicked, the input URLs and request name is checked against 
+    # Creates a new instance of a screenshotreq with data submitted from
+    # the form in new.html.erb. When the "submit request" button on the
+    # form is clicked, the input URLs and request name is checked against
     # the model validation criteria.
 
     if @screenshotreq.save
@@ -37,7 +37,7 @@ class ScreenshotreqsController < ApplicationController
 
   def show
     @screenshotreq = Screenshotreq.find(params[:id])
-    # Show method finds saved screenshotreq data and sends to a new 
+    # Show method finds saved screenshotreq data and sends to a new
     # view configured in show.html.erb.
   end
 
@@ -45,7 +45,7 @@ class ScreenshotreqsController < ApplicationController
     @screenshotreq = Screenshotreq.find(params[:id])
     @screenshotreq.destroy
     # Delete saved screenshotreq.
-   
+
     redirect_to root_path
     # Redirect view to home page after delete.
 
@@ -53,18 +53,20 @@ class ScreenshotreqsController < ApplicationController
   end
 
   private
-  # private methods are not accessible from calls beyond this 
+
+  # private methods are not accessible from calls beyond this
   # controller.
 
   def screenshotreq_params
-    params.require(:screenshotreq).permit(:name, :urls)
-    # Enable the create method to save screenshot requests with 
+    params.require(:screenshotreq)
+          .permit(:name, :urls)
+    # Enable the create method to save screenshot requests with
     # name and urls params.
   end
 
   def parse_urls
-    # parse_urls method that takes an input of a string of semicolon 
-    # delimited URLs, and assigns each individual URL in the list to 
+    # parse_urls method that takes an input of a string of semicolon
+    # delimited URLs, and assigns each individual URL in the list to
     # a separate Screenshot instance.
 
     urls = []
@@ -76,38 +78,49 @@ class ScreenshotreqsController < ApplicationController
 
     counter = 1
     urls_split.each do |url|
-      image_path = "./storage/req#{@screenshotreq.id}shot#{counter}.png"
+      image_file_path = "./storage"
       image_file_name = "req#{@screenshotreq.id}shot#{counter}.png"
-      @screenshot = Screenshot.create(url: url, image_file_path: image_path, image_file_name: image_file_name)
+      @screenshot = Screenshot.create(url: url,
+                                      image_file_path: image_file_path,
+                                      image_file_name: image_file_name)
       @screenshot.screenshotreq = @screenshotreq
       @screenshot.save!
       counter += 1
     end
-    # saves each individual URL from the list of URLs into separate 
-    # Screenshot instances. Url is the parameter used by the screenshot 
-    # capture method which determines the webpage to screenshot, image_file_path 
+    # saves each individual URL from the list of URLs into separate
+    # Screenshot instances. Url is the parameter used by the screenshot
+    # capture method which determines the webpage to screenshot, image_file_path
     # is the location where the image file of the screenshot will be stored, and
     # image_file_name is the name of the image file.
   end
 
   def create_screenshots
-    # create_screenshots uses helper methods provided by the Webshot Gem 
+    # create_screenshots uses helper methods provided by the Webshot Gem
     # to obtain image files of screenshots at specified URLs.
 
-    ws = Webshot::Screenshot.instance
+    #ws = Webshot::Screenshot.instance
     # Instantiate an instance of Webshot.
 
     @screenshotreq.screenshots.each do |screenshot|
-      ws.capture screenshot.url, screenshot.image_file_path, width: 1024, height: 768
-      screenshot.image.attach(io: File.open(screenshot.image_file_path), filename: screenshot.image_file_name)
-      # Use the ws.capture helper method to capture each screenshot 
-      # associated with the screenshotreq instance. Inputs for the 
-      # capture include the screenshot URL and the image path where 
-      # the image will be saved. Finally, the new screenshot image is 
+      RubyWebshot.call(screenshot.url,
+                       save_file_path: screenshot.image_file_path, 
+                       file_name: screenshot.image_file_name, 
+                       width: 1024, 
+                       height: 768)
+      #ws.capture screenshot.url,
+                 #screenshot.image_file_path,
+                 #width: 1024,
+                 #height: 768
+      screenshot.image.attach(io: File.open("#{screenshot.image_file_path}/#{screenshot.image_file_name}"),
+                              filename: screenshot.image_file_name)
+      # Use the ws.capture helper method to capture each screenshot
+      # associated with the screenshotreq instance. Inputs for the
+      # capture include the screenshot URL and the image path where
+      # the image will be saved. Finally, the new screenshot image is
       # associated with the screenshot instance using the .attach method
       # native to Rails Active Storage.
     end
 
-    flash[:notice] = 'The screenshots have been caputured and saved.'
+    flash[:notice] = 'The screenshots have been captured and saved.'
   end
 end
